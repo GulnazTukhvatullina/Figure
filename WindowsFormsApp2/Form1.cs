f0eb5805c29fa3766929f12733251346d67b7edf
gulnaz.tukhvatullina.02@inbox.ru <gulnaz.tukhvatullina.02@inbox.ru>
2021-04-21 13:15:12 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        private Form2 form2;
+        private Form4 form4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            form2 = new Form2();
+            //form2.Show();
+            if (form2.ShowDialog(this) == DialogResult.OK)
+            {
+                var paper = panel1.CreateGraphics();
+                var pen = new Pen(Color.Blue, 5);
+                paper.DrawEllipse(pen, 40, 40, 50, 50);
+            }
+        }
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            if (form2 != null)
+                form2.BackColor = Color.Azure;
+        }
+
+        private void Line_Click(object sender, EventArgs e)
+        {
+            form4 = new Form4();
+            //form4.Show();
+            if (form4.ShowDialog(this) == DialogResult.OK)
+            {
+                var paper = panel1.CreateGraphics();
+                var pen = new Pen(Color.Blue, 5);
+                paper.DrawLine(new Pen(Color.Green, 6), new Point(60, 30), new Point(150, 70));
+            }
+        }
+        private void Line_MouseHover(object sender, EventArgs e)
+        {
+            if (form4 != null)
+                form4.BackColor = Color.Azure;
+        }
+    }
+}
---
+++ WindowsFormsApp2/Form2.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ок = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(39, 79);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 32);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(34, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ок
+            // 
+            this.ок.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.ок.Location = new System.Drawing.Point(479, 87);
+            this.ок.Name = "ок";
+            this.ок.Size = new System.Drawing.Size(75, 23);
+            this.ок.TabIndex = 7;
+            this.ок.Text = "OK\r\n";
+            this.ок.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ок);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ок;
+    }
+}
